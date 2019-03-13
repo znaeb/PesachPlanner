2d2bb4cbce1662cf65f9630d948038f6c639c31e
Benjamin Nimchinsky <urnotbeanz@gmail.com>
2019-03-13 13:19:03 -0500

    Initial commit

---
+++ php/homePage.php
@@ -0,0 +1,193 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benjamin
+ * Date: 3/10/2016
+ * Time: 3:14 PM
+ */
+ //header("Location: submit.php?url=pop");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pesach Planner</title>
+    <link rel="shortcut icon"
+          href="../images/regular_smile.gif" />
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link rel='stylesheet' href='../css/Style.css' type='text/css' media='all'/>
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <?php
+        include "_navbar.php";
+    ?>
+    <div id="content" class="content">
+        <header class="entry-header">
+            <img alt="" border="0" src="../images/YIH-logo.gif" style="width: 100px; height: 99px; float:right;">
+            <h1 class="entry-title">Pesach Planner</h1>
+        </header>
+        <div class="DisplayWords">
+            <hr>
+            <p>
+                The&nbsp;<a href="http://yihouston.org/" target="_blank"><strong>Young Israel of Houston</strong></a>&nbsp;
+                <strong><em>Pesach Planner</em></strong>&nbsp;is produced as a service to the&nbsp;community.&nbsp;It is published
+                under the auspices of Rabbi Yehoshua Wender&nbsp;of&nbsp;Young Israel of Houston, and is a collaborative work
+                by&nbsp;<span id="cloak31579"><a href="mailto:rw@yihouston.org?subject=YIH%20Pesach%20Planner" target="_blank">
+                            Rabbi Yehoshua Wender</a></span><script type="text/javascript">
+                    //<!--
+                    document.getElementById('cloak31579').innerHTML = '';
+                    var prefix = '&#109;a' + 'i&#108;' + '&#116;o';
+                    var path = 'hr' + 'ef' + '=';
+                    var addy31579 = 'rw' + '&#64;';
+                    addy31579 = addy31579 + 'y&#105;h&#111;&#117;st&#111;n' + '&#46;' + '&#111;rg?s&#117;bj&#101;ct=YIH%20P&#101;s&#97;ch%20Pl&#97;nn&#101;r';
+                    var addy_text31579 = 'R&#97;bb&#105; Y&#101;h&#111;sh&#117;&#97; W&#101;nd&#101;r';
+                    document.getElementById('cloak31579').innerHTML += '<a ' + path + '\'' + prefix + ':' + addy31579 + '\' target="_blank">'+addy_text31579+'<\/a>';
+                    //-->
+                </script>&nbsp;and&nbsp;<span id="cloak49122"><a href="mailto:rivkey@gmail.com?subject=YIH%20Pesach%20Planner" target="_blank">Mrs. Rivkey Nimchinsky.</a></span><script type="text/javascript">
+                    //<!--
+                    document.getElementById('cloak49122').innerHTML = '';
+                    var prefix = '&#109;a' + 'i&#108;' + '&#116;o';
+                    var path = 'hr' + 'ef' + '=';
+                    var addy49122 = 'r&#105;vk&#101;y' + '&#64;';
+                    addy49122 = addy49122 + 'gm&#97;&#105;l' + '&#46;' + 'c&#111;m?s&#117;bj&#101;ct=YIH%20P&#101;s&#97;ch%20Pl&#97;nn&#101;r';
+                    var addy_text49122 = 'Mrs. R&#105;vk&#101;y N&#105;mch&#105;nsky.';
+                    document.getElementById('cloak49122').innerHTML += '<a ' + path + '\'' + prefix + ':' + addy49122 + '\' target="_blank">'+addy_text49122+'<\/a>';
+                    //-->
+                </script></p>
+            <div style="text-align: center;">
+                &nbsp;</div>
+            <h2 style="text-align: center;">
+                <span style="color:#800080;"><span style="font-family:trebuchet ms,helvetica,sans-serif;">We are now accepting ads for the 2019 Pesach Planner.</span></span></h2>
+            <h2 style="text-align: center;">
+                <span style="color:#2f4f4f;"><span style="font-family:trebuchet ms,helvetica,sans-serif;"><a href="http://www.rivkey.com/PDFs/yih%202019%20pp-ad%20letter.pdf" target="_blank">Please click here for more information on placing your ad.</a></span></span></h2>
+            <p>
+                &nbsp;</p>
+            <h2>
+                <span style="color:#006400;"><span style="font-family:trebuchet ms,helvetica,sans-serif;">We are currently hard at work on the 2019 Pesach Planner.&nbsp;&nbsp;</span></span></h2>
+            <h2>
+                <span style="color:#006400;"><span style="font-family:trebuchet ms,helvetica,sans-serif;">Our 2019 /5779 Pesach Planner will be Gd willing available before Rosh Chodesh Nissan.</span></span></h2>
+            <p>
+                &nbsp;</p>
+            <h2>
+                <span style="color:#b22222;"><span style="font-family: &quot;trebuchet ms&quot;, helvetica, sans-serif;">The following documents can help get you started with your preparations, but should be used for reference only. Things may change from year to year.</span></span></h2>
+            <div style="text-align: center;">
+                &nbsp;</div>
+            <span style="display: none;">&nbsp;</span>
+            <h3>
+                <span style="color:#800000;"><strong style="color: rgb(255, 0, 0); font-family: arial, helvetica, sans-serif;"><span style="font-size: 12px;"><em><span id="cke_bm_155S" style="display: none;">&nbsp;</span>(Please note that viewing these files in Google Docs may skew the formatting.)<span id="cke_bm_155E" style="display: none;">&nbsp;</span></em></span></strong></span></h3>
+            <span style="display: none;">&nbsp;</span>
+            <div>
+                <div>
+                    <table border="0" cellpadding="0" cellspacing="0" style="width: 500px;" align="center">
+                        <tbody>
+                        <tr>
+                            <td style="width: 257px;">
+                                <p>
+                                    <a href="http://www.rivkey.com/PDFs/YIH%20PP%202018-5778.pdf" target="_blank"><img alt="" src="../images/cover-2018-thumbnail.gif" style="width: 250px; height: 193px;"></a></p>
+                                <p style="text-align: center;">
+                                    <strong style="font-size: 16px; color: rgb(178, 34, 34); font-family: arial, helvetica, sans-serif;"><a href="http://www.rivkey.com/PDFs/YIH%20PP%202018-5778.pdf" target="_blank">2018 / 5778 YIH Pesach Planner</a></strong></p>
+                                <div style="text-align: center;">
+                                    <strong style="font-size: 11px; font-family: arial, helvetica, sans-serif;">Published online for 2018, on Erev Shabbos<span style="color: rgb(255, 0, 0);">&nbsp;</span></strong></div>
+                                <div style="text-align: center;">
+                                    <strong style="font-size: 11px; font-family: arial, helvetica, sans-serif;"><span style="color: rgb(255, 0, 0);">3-16-2018 @ 1:00 PM cdt</span></strong></div>
+                            </td>
+                            <td style="width: 0px;">
+                                &nbsp;</td>
+                            <td>
+                                <p style="text-align: center;">
+                                    <font face="arial, helvetica, sans-serif"><span style="font-size: 11px;"><b><a href="http://www.rivkey.com/PDFs/YIH%20PP%202018%20updates.pdf" target="_blank"><img alt="" class="cke-resize" src="../images/2018Updates4Thumb.gif" style="width: 250px; height: 193px;"></a></b></span></font></p>
+                                <p style="text-align: center;">
+                                    &nbsp;</p>
+                                <p style="text-align: center;">
+                                    <span style="color:#006400;"><font face="arial, helvetica, sans-serif"><span style="font-size: 11px;"><b>Last update: March 27 @12:01 AM cdt</b></span></font></span></p>
+                                <p style="text-align: center;">
+                                    <font face="arial, helvetica, sans-serif"><span style="font-size: 11px;"><b>&nbsp;&nbsp;<img alt="smiley" height="20" src="../images/regular_smile.gif" title="smiley" width="20"></b></span></font></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 257px;">
+                                <p>
+                                    <a href="http://www.rivkey.com/PDFs/YIH-pp-2018-schedule.pdf" style="text-align: center;" target="_blank"><img alt="" class="cke-resize cke-resize cke-resize" src="../images/thumb-schedule-2015.gif" style="width: 250px; height: 193px;"></a></p>
+                                <div style="text-align: center;">
+                                    <span style="color:#0000cd;"><a href="http://www.rivkey.com/PDFs/YIH-pp-2018-schedule.pdf" target="_blank"><strong style="font-family: arial, helvetica, sans-serif; font-size: 16px; color: rgb(178, 34, 34);">2018 / 5778</strong></a></span></div>
+                                <div style="text-align: center;">
+                                    <span style="color:#0000cd;"><strong style="font-family: arial, helvetica, sans-serif; font-size: 16px; color: rgb(178, 34, 34);"><a href="http://www.rivkey.com/PDFs/YIH-pp-2018-schedule.pdf" target="_blank">YIHouston&nbsp;Pesach Schedule</a></strong></span></div>
+                                <div style="text-align: center;">
+                                    <strong style="color: rgb(0, 128, 0); font-size: 11px; font-family: arial, helvetica, sans-serif;">Last update: 3-25-2018 @ 6:00 AM cdt</strong></div>
+                                <div style="text-align: center;">
+                                    &nbsp;</div>
+                                <div style="text-align: center;">
+                                    &nbsp;</div>
+                            </td>
+                            <td style="width: 0px;">
+                                &nbsp;</td>
+                            <td>
+                                <div style="text-align: center;">
+                                    <h3>
+                                        <a href="http://www.rivkey.com/PDFs/YIH-pp-2018-SaleOfChametz.pdf" style="font-size: 13px; font-weight: normal;" target="_blank"><img alt="" class="cke-resize cke-resize cke-resize cke-resize cke-resize" src="../images/thumb-saleofchametz-2015.gif" style="width: 250px; height: 193px;"></a></h3>
+                                    <div style="font-size: 13px;">
+                                        <h3>
+                                            <span style="color: rgb(0, 0, 205);"><a href="http://www.rivkey.com/PDFs/YIH-pp-2018-SaleOfChametz.pdf" target="_blank"><strong style="font-family: arial, helvetica, sans-serif; font-size: 16px; color: rgb(178, 34, 34);">2018 / 5778</strong></a></span></h3>
+                                    </div>
+                                    <div style="font-size: 13px;">
+                                        <span style="color: rgb(0, 0, 205);"><strong style="font-family: arial, helvetica, sans-serif; font-size: 16px; color: rgb(178, 34, 34);"><a href="http://www.rivkey.com/PDFs/YIH-pp-2018-SaleOfChametz.pdf" target="_blank">YIHouston Sale of Chametz</a></strong></span></div>
+                                    <div style="font-size: 13px;">
+                                        <span style="color: rgb(0, 100, 0);"><strong style="font-size: 10px;"><span style="font-family: arial, helvetica, sans-serif;"><span style="font-size: 11px;">&nbsp;</span></span></strong><span style="font-size: 11px;">For those planning on selling their Chametz through Rabbi Wender, one should meet with the Rabbi in person at some point before Mincha on Thursday, March 29<sup>th</sup></span></span></div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 257px;">
+                                <br>
+                                <div style="text-align: center;">
+                                    <a href="http://jhvonline.com/pesach-planner-turns-p16962-96.htm" target="_blank"><input alt="" src="../images/JHV-4link.gif" style="width: 150px; height: 28px;" type="image"></a></div>
+                                <div style="text-align: center;">
+                                    &nbsp;</div>
+                                <div style="text-align: center;">
+                                    <strong style="font-size: 16px; color: rgb(178, 34, 34); font-family: arial, helvetica, sans-serif;"><a href="http://jhvonline.com/pesach-planner-turns-p16962-96.htm" target="_blank">Link to Jewish Herald Voice article</a>&nbsp;</strong><a href="http://jhvonline.com/pesach-planner-turns-p16962-96.htm" target="_blank"><strong style="font-size: 16px; color: rgb(178, 34, 34); font-family: arial, helvetica, sans-serif;">regarding Pesach Planner</strong></a></div>
+                                <div style="text-align: center;">
+                                    &nbsp;</div>
+                                <div style="text-align: center;">
+                                    <strong style="font-size: 11px; font-family: arial, helvetica, sans-serif; color: rgb(0, 0, 0);">Article published in the Jewish Herald Voice</strong></div>
+                                <div style="text-align: center;">
+                                    <span style="color: rgb(0, 0, 0);"><span style="font-family: arial, helvetica, sans-serif;"><span style="font-size: 11px;"><strong>on March 27, 2014,</strong></span></span></span></div>
+                                <div style="text-align: center;">
+                                    <span style="color: rgb(0, 0, 0);"><span style="font-family: arial, helvetica, sans-serif;"><span style="font-size: 11px;"><strong>showcasing our Pesach Planner</strong></span></span></span></div>
+                                <div>
+                                    &nbsp;</div>
+                            </td>
+                            <td style="width: 0px;">
+                                &nbsp;</td>
+                            <td>
+                                <div style="text-align: center;">
+                                    <h3>
+                                        <strong style="font-size: 16px; color: rgb(178, 34, 34); font-family: arial, helvetica, sans-serif;"><a href="http://pesachplanner.org/index.php/sefirat-haomer">Count Sefirat HaOmer:</a></strong></h3>
+                                    <p>
+                                        <strong style="font-size: 16px; color: rgb(178, 34, 34); font-family: arial, helvetica, sans-serif;"><a href="http://pesachplanner.org/index.php/sefirat-haomer"><input alt="" src="../images/calendar3.gif" style="width: 133px; height: 135px;" type="image"></a></strong></p>
+                                </div>
+                                <h3 style="text-align: center;">
+                                    <strong style="font-size: 16px; color: rgb(178, 34, 34); font-family: arial, helvetica, sans-serif;"><a href="http://pesachplanner.org/index.php/sefirat-haomer">2018 links</a></strong></h3>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <p>
+                        &nbsp;</p>
+                </div>
+                <p>
+                    <strong class="center" style="font-family: Arial, &#39;Arial Unicode MS&#39;, Helvetica, sans-serif; font-size: 13px;"><img alt="" border="0" src="../images/slin6.gif" style="width: 585px; height: 33px;"></strong></p>
+            </div>
+        </div>
+    </div>
+    <aside id="sidebar" class="sidebar">
+        <li><span>
+        If you are using Explorer and are having difficulty opening our files, please do the following:
+                (1) Right click on the link or image. (2) Select "Save Target As". (3) Save the file.</span></li>
+        <li><span>
+        If you are using any browser and have difficulty opening our files, simply right-click on the link and download
+            the PDF file directly to your computer as you would any other download.</span>
+        </li>
+    </aside>
+</body>
+</html>
